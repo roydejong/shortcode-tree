c22b62b44852336df3e2f61f88fabad7f4d2d896
Roy de Jong <roy@softwarepunt.nl>
2017-11-20 12:57:26 +0100

    Extractor: Basic, WIP support for "unsticking" detected shortcodes

--- src/shortcode-extractor.js
+++ src/shortcode-extractor.js
@@ -5,19 +5,20 @@ let ShortcodeParser = require('./shortcode-parser');
  */
 let ShortcodeExtractor = {
     /**
-     * Extracts all shortcode blocks from an input text, one level deep.
+     * Extract and parse all shortcodes from input text, but only one level deep.
      *
      * @param {string} text
-     * @returns {Array}
+     * @returns {Shortcode[]|array}
      */
-    extractShortcodeTexts(text) {
+    extractShortcodes(text) {
         // Perform a generic regex match that will match any block spanning [XXX] to [/XXX].
         // Because it is a generic regex, this may also result in a matching multiple blocks on the same level; e.g. [XXX]bla[/XXX][YYY]bla2[/YYY].
         // Worse yet, in the case of malformatted shortcodes, this won't work reliably at all. That's a TODO: Figure out how to deal with malformatted input elegantly
         const regex = /\[(.*)\]([\S\s]*)\[\/(.*)\]/g;
 
-        let set = [];
         let m;
+
+        let matches = [];
 
         while ((m = regex.exec(text)) !== null) {
             if (m.index === regex.lastIndex) {
@@ -26,13 +27,33 @@ let ShortcodeExtractor = {
 
             m.forEach((match, groupIndex) => {
                 if (groupIndex === 0) {
-                    set.push(match);
+                    matches = matches.concat(this.reduceShortcodeMatch(match));
                 }
             });
         }
 
-        return set;
+        return matches;
     },
+
+    /**
+     * Attempts to "unstick" multiple blocks matched in one piece of text.
+     *
+     * @param {string} matchText
+     * @returns {array}
+     */
+    reduceShortcodeMatch (matchText) {
+        let results = [];
+        let remainingText = matchText;
+
+        while (remainingText.length > 0) {
+            let parsed = ShortcodeParser.parseShortcode(remainingText);
+            results.push(parsed);
+
+            remainingText = remainingText.substr(parsed.codeText.length);
+        }
+
+        return results;
+    }
 };
 
 module.exports = ShortcodeExtractor;
--- test/shortcode-extractor.test.js
+++ test/shortcode-extractor.test.js
@@ -1,22 +1,36 @@
 let ShortcodeExtractor = require('../src').ShortcodeExtractor;
+let Shortcode = require('../src').Shortcode;
 
 let {expect} = require('chai');
 
-describe('ShortcodeExtractor.extractShortcodeTexts()', function () {
+describe('ShortcodeExtractor.extractShortcodes()', function () {
     it('extracts a simple shortcode', function () {
         let testInput = "[b]bold text[/b]";
-        let expectedOutput = [testInput];
+        let expectedOutput = [new Shortcode("b", "bold text", {}, false, testInput)];
 
-        let actualOutput = ShortcodeExtractor.extractShortcodeTexts(testInput) || null;
+        let actualOutput = ShortcodeExtractor.extractShortcodes(testInput) || null;
 
         expect(actualOutput).to.deep.equal(expectedOutput);
     });
 
     it('extracts a simple shortcode with text surrounding it', function () {
-        let testInput = "Check out [b]bold text[/b], it's pretty cool.";
-        let expectedOutput = ["[b]bold text[/b]"];
+        let testInput = "Check out [b]bold wrap text[/b], it's pretty cool.";
+        let expectedOutput = [new Shortcode("b", "bold wrap text", {}, false, "[b]bold wrap text[/b]")];
 
-        let actualOutput = ShortcodeExtractor.extractShortcodeTexts(testInput) || null;
+        let actualOutput = ShortcodeExtractor.extractShortcodes(testInput) || null;
+
+        expect(actualOutput).to.deep.equal(expectedOutput);
+    });
+
+    it('extracts multiple simple shortcodes on the same level', function () {
+        let testInput = "[b]boldest text[/b][i]bats[/i]";
+
+        let expectedOutput = [
+            new Shortcode("b", "boldest text", {}, false, "[b]boldest text[/b]"),
+            new Shortcode("i", "bats", {}, false, "[i]bats[/i]")
+        ];
+
+        let actualOutput = ShortcodeExtractor.extractShortcodes(testInput) || null;
 
         expect(actualOutput).to.deep.equal(expectedOutput);
     });
