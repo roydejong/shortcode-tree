71d12ea1d2fa21b2a69a7f798db6be537e5026a3
Roy de Jong <roy@softwarepunt.nl>
2017-11-21 11:49:39 +0100

    Nodes: Add debug trace functions

--- src/shortcode-node.js
+++ src/shortcode-node.js
@@ -16,6 +16,10 @@ class ShortcodeNode {
         this.children = [];
     }
 
+    isRootNode() {
+        return !this.hasShortcode();
+    }
+
     hasShortcode() {
         return this.shortcode != null;
     }
@@ -30,6 +34,39 @@ class ShortcodeNode {
 
     hasChildren() {
         return this.children.length > 0;
+    }
+
+    traceTree(level) {
+        if (!level) {
+            level = 0;
+        }
+
+        let fnRepeatChar = function (char, level) {
+            if (level === 0) {
+                return "";
+            }
+
+            let chars = "";
+
+            for (let i = 0; i < (level * 3); i++) {
+                chars += char;
+            }
+
+            return chars + " ";
+        };
+
+        if (this.isRootNode()) {
+            console.log('+++++ Root Node +++++');
+            level = 0;
+        } else {
+            console.log(fnRepeatChar('-', level) + '[Shortcode Node: ' + this.shortcode.name + '], content: ' + this.shortcode.content);
+            level++;
+        }
+
+        for (let i = 0; i < this.children.length; i++) {
+            let childNode = this.children[i];
+            childNode.traceTree(level);
+        }
     }
 }
 
--- src/text-node.js
+++ src/text-node.js
@@ -2,6 +2,28 @@ class TextNode {
     constructor (text) {
         this.text = text;
     }
+
+    traceTree(level) {
+        if (!level) {
+            level = 0;
+        }
+
+        let fnRepeatChar = function (char, level) {
+            if (level === 0) {
+                return "";
+            }
+
+            let chars = "";
+
+            for (let i = 0; i < (level * 3); i++) {
+                chars += char;
+            }
+
+            return chars + " ";
+        };
+
+        console.log(fnRepeatChar('-', level) + '[Text Node], content: ' + this.text);
+    }
 }
 
 module.exports = TextNode;
