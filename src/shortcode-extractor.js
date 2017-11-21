94d6041dffd50ba900e1b7162e7f0bb57c832087
Roy de Jong <roy@softwarepunt.nl>
2017-11-21 11:04:11 +0100

    Extractor: Support passing custom parser options

--- src/shortcode-extractor.js
+++ src/shortcode-extractor.js
@@ -8,9 +8,10 @@ let ShortcodeExtractor = {
      * Extract and parse all shortcodes from input text, but only one level deep.
      *
      * @param {string} text
+     * @param {object} options (Optional) Parser options
      * @returns {Shortcode[]|array}
      */
-    extractShortcodes(text) {
+    extractShortcodes(text, options) {
         // Perform a generic regex match that will match any block spanning [XXX] to [/XXX].
         // Because it is a generic regex, this may also result in a matching multiple blocks on the same level; e.g. [XXX]bla[/XXX][YYY]bla2[/YYY].
         // Worse yet, in the case of malformatted shortcodes, this won't work reliably at all. That's a TODO: Figure out how to deal with malformatted input elegantly
@@ -24,7 +25,7 @@ let ShortcodeExtractor = {
                 regex.lastIndex++;
             }
 
-            resultSet = resultSet.concat(this.reduceShortcodeMatch(text, match.index));
+            resultSet = resultSet.concat(this.reduceShortcodeMatch(text, match.index, options));
         }
 
         return resultSet;
@@ -35,13 +36,19 @@ let ShortcodeExtractor = {
      *
      * @param {string} text
      * @param {int} offset
+     * @param {object} options (Optional) Parser options
      * @returns {array}
      */
-    reduceShortcodeMatch(text, offset) {
+    reduceShortcodeMatch(text, offset, options) {
         let results = [];
 
         while (offset < text.length) {
-            let parsedShortcode = ShortcodeParser.parseShortcode(text, {offset: offset, throwErrors: false});
+            let optionsMerged = Object.assign({}, options, {
+                offset: offset,
+                throwErrors: false
+            });
+
+            let parsedShortcode = ShortcodeParser.parseShortcode(text, optionsMerged);
 
             if (!parsedShortcode) {
                 // Parse error
